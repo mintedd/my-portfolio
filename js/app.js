fe93bcbbc81e8f53509a667c4c2872eb0aadeb43
mintedd <xxxxiheartyouxxxxx@gmail.com>
2022-11-13 21:22:05 -0500

    added js to split up intro and homepage interface

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 $(document).foundation()
+
+function init(){
+    $('.homepage').hide();
+}
+init();
+
+var homepage = document.getElementById('myWork')
+
+function loadPage(){ 
+    $('.homepage').show();
+    $('.hero').hide();
+
+}
+homepage.addEventListener('click',loadPage)
